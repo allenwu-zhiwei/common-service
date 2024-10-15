ae0359232db448498f8e569c88d969b5d3de3077
allenwu <allenzhiwei0@gmail.com>
2024-10-15 16:31:24 +0800

    add log getCurrentInfo method

--- src/main/java/com/nusiss/commonservice/feign/UserFeignClient.java
+++ src/main/java/com/nusiss/commonservice/feign/UserFeignClient.java
@@ -4,12 +4,16 @@ import com.nusiss.commonservice.config.ApiResponse;
 import com.nusiss.commonservice.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "user-service")
 public interface UserFeignClient {
 
     @GetMapping("/api/user/{id}")
     ResponseEntity<ApiResponse<User>> getUserById(@PathVariable("id") Integer id);
+
+
+    @RequestMapping(value = "/getCurrentUserInfo", method = RequestMethod.POST)
+    public ResponseEntity<ApiResponse<User>> getCurrentUserInfo(@RequestHeader("authToken") String authToken);
+
 }
